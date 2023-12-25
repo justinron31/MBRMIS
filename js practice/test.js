162b84ee776348f5a4a2c3b876a03fb1b036772d
K1dla <justingalangzxc31@gmail.com>
2023-12-25 20:34:26 +0800

    same

---
+++ js practice/test.js
@@ -0,0 +1,16 @@
+let g1 = 90;
+let g2 = 85;
+let g3 = 96.4;
+let g4 = 88.02;
+let g5 = 97.3; 
+
+let ave = (g1 + g2 + g3 + g4 + g5) / 5;
+alert ( " ENGISH: " + g1 + " \n MATH: " + g2 + "\n SCIENCE: " + g3 + "\n SHEESH: " + g4 + "\n PE: " + g5 + "\n\n AVERAGE: " + ave);
+
+console.log(ave);
+
+
+
+
+
+
