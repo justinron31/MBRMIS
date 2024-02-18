bf7ca26070fdc7ebc8d639b8df36c186941cbcd4
K1dla <justingalangzxc31@gmail.com>
2024-02-19 00:33:43 +0800

    updated logout php bro bro

--- Php/logout.php
+++ Php/logout.php
@@ -1,4 +1,19 @@
 <?php
+
+include 'db.php';
 session_start();
+$user_id = $_SESSION['user_id']; // assuming you stored user_id in session when user logged in
+
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Update is_logged_in to 0
+$updateLogoutStatus = $conn->prepare("UPDATE staff SET is_logged_in = 0 WHERE id = ?");
+$updateLogoutStatus->bind_param("i", $user_id);
+$updateLogoutStatus->execute();
+
 session_destroy();
 echo json_encode(['success' => true]);
